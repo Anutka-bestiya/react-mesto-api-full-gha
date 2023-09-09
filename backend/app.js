7841dc021b902bc1ffe001897136217cb22402b5
Anutka_bestiya <anchikfyz@yandex.ru>
2023-09-10 02:03:38 +0500

    ref

--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,7 @@ const allowedCors = [
 ];
 
 app.use(cors(
-  { origin: allowedCors, credentials: true, methods: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH'] },
+  { origin: allowedCors, credentials: true, allowedHeaders: ['Content-Type', 'Authorization'] },
 ));
 
 app.use(helmet());
