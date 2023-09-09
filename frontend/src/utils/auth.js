fd293c161834810c1dffec912d0d7f85a62ded13
Anutka_bestiya <anchikfyz@yandex.ru>
2023-09-10 00:32:16 +0500

    ref 4000/3000

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:4000';
+export const BASE_URL = 'https://localhost:3000';
 
 function checkRes(res) {
   return res.ok ? res.json() : Promise.reject(res.status);
