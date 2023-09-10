1c1b49d051889a7c7db0e10f0b70e34e467c4cbc
Anutka_bestiya <anchikfyz@yandex.ru>
2023-09-10 18:30:29 +0500

    ref

--- backend/middlewares/centralized-error-handler.js
+++ backend/middlewares/centralized-error-handler.js
@@ -1,9 +1,14 @@
 // eslint-disable-next-line consistent-return
-const centralizedErrorHandler = (err, req, res) => {
+const centralizedErrorHandler = (err, req, res, next) => {
   // если у ошибки нет статуса, выставляем 500
+  console.log(err);
   let { statusCode } = err;
+  console.log(statusCode);
   const { message } = err;
+  console.log(message);
   if (!statusCode) { statusCode = 500; }
+  console.log(statusCode);
+  console.log(res);
   return res
     .status(statusCode)
     .send(
