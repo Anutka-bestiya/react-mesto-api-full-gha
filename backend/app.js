a5f69d582791cfb0955047c9f6fd49704f37f022
Anutka_bestiya <anchikfyz@yandex.ru>
2023-09-10 17:52:24 +0500

    ref revui1

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const routes = require('./routes/router');
 const centralizedErrorHandler = require('./middlewares/centralized-error-handler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 4000 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 
